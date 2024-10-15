85ad50b73dbe2db953d7c99f0a4ba0fb027c3d90
MiIyama <naomi898@gmail.com>
2024-10-15 13:46:52 -0300

    refactor: Atualizar _app.tsx para adicionar suporte a tema escuro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
-import '../styles/global.css';
-
+import { FormControlLabel, Switch } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import type { PaletteMode } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
+import * as React from 'react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const getDesignTokens = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+  },
+});
 
-export default MyApp;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
+  const handleThemeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMode(event.target.checked ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <FormControlLabel
+        control={
+          <Switch checked={mode === 'dark'} onChange={handleThemeChange} />
+        }
+        label="Dark Mode"
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
