e72af77e36619593584d9e7809eb21bacd2dc2a3
MiIyama <naomi898@gmail.com>
2024-10-21 14:02:20 -0300

    refactor: adapta props da img e layout no Hero

--- src/content/content.js
+++ src/content/content.js
@@ -1,14 +1,25 @@
+/* eslint-disable prettier/prettier */
 const content = {
   hero: {
+    layout: {
+      direction: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
     headerSlot: 'szdszdszdzdzd',
     title: 'Adapting and Thriving in a Changing World',
-    subtitle:
-      'Our comprehensive guide to strategies for entrepreneurs and leaders offers valuable insights, practical advice.',
+    subtitle:'Our comprehensive guide to strategies for entrepreneurs and leaders offers valuable insights, practical advice.',
     buttons: {
       items: [
         { label: 'Get Started', variant: 'contained' },
         { label: 'Learn More', variant: 'outlined' },
       ],
+    },
+    image: {
+      src: 'https://picsum.photos/id/56/3000/2000',
+      width: 1152,
+      height: 300,
+      alt: 'Random Image',
     },
   },
 };
--- src/library/components/molecules/ContentBlock.js
+++ src/library/components/molecules/ContentBlock.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import PropTypes from 'prop-types';
 import React from 'react';
 import Subtitle from '@/library/components/atoms/Subtitle';
@@ -7,126 +6,81 @@ import { Stack, Box } from '@mui/material';
 import Button from '@/library/components/atoms/Button';
 import Image from 'next/image';
 
-const stackOptions = {
-  direction: ['column', 'row'],
-  justifyContent: ['center'],
-  alignItems: ['flex-start', 'center', 'flex-end'],
-};
-
-// Combinações a serem evitadas (opcional)
-const excludedCombinations = [
-  { direction: 'row', justifyContent: 'flex-start', alignItems: 'flex-start' },
-];
-
-// Função para verificar se uma combinação deve ser excluída
-const isExcluded = (direction, justifyContent, alignItems) =>
-  excludedCombinations.some(
-    (comb) =>
-      comb.direction === direction &&
-      comb.justifyContent === justifyContent &&
-      comb.alignItems === alignItems
-  );
-
 const ContentBlock = ({ content }) => {
+  const {
+    layout: { direction, justifyContent, alignItems },
+    headerSlot,
+    title,
+    subtitle,
+    buttons,
+    image,
+  } = content;
+
   return (
     <>
-      {stackOptions.direction.map((direction) =>
-        stackOptions.justifyContent.map((justifyContent) =>
-          stackOptions.alignItems.map((alignItems, index) => {
-            if (isExcluded(direction, justifyContent, alignItems)) return null;
-
-            return (
-              <Box
-                sx={{
-                  border: '1px solid black',
-                }}
-                key={`${direction}-${justifyContent}-${alignItems}-${index}`}
-              >
-                <Box
-                  sx={{
-                    border: '1px solid red',
-                    padding: '10px',
-                    mt: 1,
-                    mb: 10,
-                  }}
-                >
-                  Direction: {direction}, Justify: {justifyContent}, Align:{' '}
-                  {alignItems}
-                </Box>
-
-                <Stack
-                  direction={direction}
-                  sx={{
-                    justifyContent,
-                    alignItems,
-                    padding: '10px',
-                    marginBottom: '20px',
-                    minHeight: '100px',
-                  }}
-                >
-                  <Box>
-                    {content.headerSlot && content.headerSlot}
-                    {/* TODO:https://chatgpt.com/c/670cb563-8794-800a-893c-57f257b4b5b2 */}
-
-                    <Title>{content.title}</Title>
-                  </Box>
-                  <Subtitle>{content.subtitle}</Subtitle>
-
-                  {content.buttons?.items?.length > 0 && (
-                    <Stack direction="row" spacing={2}>
-                      {/* {content.buttons.items.map((button, idx) => (
-                        <Button
-                          key={idx}
-                          variant={button.variant}
-                          color={button.color}
-                        >
-                          {button.label}
-                        </Button>
-                      ))} */}
-                    </Stack>
-                  )}
-                </Stack>
-
-                <Image
-                  src="https://picsum.photos/id/56/3000/2000"
-                  width={1152}
-                  height={300}
-                  alt="Random Image"
-                />
-              </Box>
-            );
-          })
-        )
-      )}
-      {/* 
-      <Stack
-        direction="row"
+      <Box
         sx={{
-          justifyContent: 'left',
-          alignItems: 'flex-end',
+          border: '1px solid black',
         }}
-        spacing={0}
       >
-        <Box>
-          {content.headerSlot}
-          TODO:https://chatgpt.com/c/670cb563-8794-800a-893c-57f257b4b5b2
-          <Title>{content.title}</Title>
+        <Box
+          sx={{
+            border: '1px solid red',
+            padding: '10px',
+            mt: 1,
+            mb: 10,
+          }}
+        >
+          Direction: {direction}, Justify: {justifyContent}, Align: {alignItems}
         </Box>
-        <Subtitle>{content.subtitle}</Subtitle>
-        <Stack direction="row" spacing={2}>
-          {content.buttons.items.map((button, index) => (
-            <Button key={index} variant={button.variant} color={button.color}>
-              {button.label}
-            </Button>
-          ))}
+
+        <Stack
+          direction={direction}
+          sx={{
+            justifyContent,
+            alignItems,
+            padding: '10px',
+            marginBottom: '20px',
+            minHeight: '100px',
+          }}
+        >
+          {headerSlot && <Box>{headerSlot}</Box>}
+          <Title>{title}</Title>
+          <Subtitle>{subtitle}</Subtitle>
+
+          {buttons?.items?.length > 0 && (
+            <Stack direction="row" spacing={2}>
+              {buttons.items.map((button, idx) => (
+                <Button key={idx} variant={button.variant} color={button.color}>
+                  {button.label}
+                </Button>
+              ))}
+            </Stack>
+          )}
+
+          {image && (
+            <Image
+              src={image.src}
+              width={image.width}
+              height={image.height}
+              alt={image.alt}
+            />
+          )}
         </Stack>
-      </Stack> */}
+      </Box>
     </>
   );
 };
 
 ContentBlock.propTypes = {
   content: PropTypes.shape({
+    layout: PropTypes.shape({
+      direction: PropTypes.oneOf(['column', 'row']).isRequired,
+      justifyContent: PropTypes.oneOf(['center', 'flex-start', 'flex-end'])
+        .isRequired,
+      alignItems: PropTypes.oneOf(['flex-start', 'center', 'flex-end'])
+        .isRequired,
+    }).isRequired,
     headerSlot: PropTypes.node,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
@@ -139,6 +93,12 @@ ContentBlock.propTypes = {
         })
       ),
     }),
+    image: PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      width: PropTypes.number.isRequired,
+      height: PropTypes.number.isRequired,
+      alt: PropTypes.string.isRequired,
+    }).isRequired,
   }).isRequired,
 };
 
