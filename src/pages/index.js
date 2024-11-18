12ecae2513d7ff2532e53a63f714bd0049a9e97a
MiIyama <44329442+MiIyama@users.noreply.github.com>
2024-11-18 19:31:31 +0000

    add Features playground

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import content from '@/content/content';
-
 import PageBuilder from '@/library/utils/PageBuilder';
+import FeaturesPlayground from '@/library/components/organisms/FeaturesPlayground';
 
-const Index = () => <PageBuilder pageContent={content} />;
+const Index = () => (
+  <>
+    {' '}
+    <FeaturesPlayground /> {/* Adiciona o playground abaixo do conteúdo principal */}
+    <PageBuilder pageContent={content} />
+  </>
+);
 
 export default Index;
